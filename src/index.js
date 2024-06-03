69dc9c172ae540a33699a95381883146d139b2f8
IsaacMasisV <isaacmv2003@gmail.com>
2024-06-03 13:26:49 -0600

    commit de lista, inpuds, boton de agregar

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 // Inserte el código aquí
 
+let lista = document.getElementById('lista1') 
+let texto = document.getElementById('inputs')
+let boton = document.getElementById("btn")
+boton.addEventListener('click',()=>{
+    
+    let bts = document.createElement("p")
+    bts.textContent=texto.value
+
+    lista.appendChild(bts)
+})
+
+
+
+
+commit de lista, inpuds, boton de agregar
