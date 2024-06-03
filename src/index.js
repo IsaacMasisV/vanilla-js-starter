deece2559f4b1cf1d2c0928b3a466ef24884b5c2
IsaacMasisV <isaacmv2003@gmail.com>
2024-06-03 14:45:07 -0600

    commit de agredar boton de borrar, checkbox, al precionar enter se agregue el elemento

--- src/index.js
+++ src/index.js
@@ -3,15 +3,35 @@
 let lista = document.getElementById('lista1') 
 let texto = document.getElementById('inputs')
 let boton = document.getElementById("btn")
+
 boton.addEventListener('click',()=>{
     
     let bts = document.createElement("p")
     bts.textContent=texto.value
 
     lista.appendChild(bts)
+
+    let caja = document.createElement("input");
+    caja.type= "checkbox"
+    
+    bts.appendChild(caja)
+ 
+    let bor = document.createElement("button");
+    bor.textContent= "Q"
+
+    bts.appendChild(bor)
+
+
+
+
+
+    
+
+   
+
+
+
 })
 
 
 
-
-commit de lista, inpuds, boton de agregar
